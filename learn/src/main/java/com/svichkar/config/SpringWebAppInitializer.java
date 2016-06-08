00461e8765862bcf89d2f6275e7a6993dd6ba0b2
konstantin_svichkar <konstantin.svichkar@nixsolutions.com>
2016-06-08 23:32:01 +0300

    in progress

--- learn/src/main/java/com/svichkar/config/SpringWebAppInitializer.java
+++ learn/src/main/java/com/svichkar/config/SpringWebAppInitializer.java
@@ -1,8 +1,13 @@
 package com.svichkar.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -12,15 +17,20 @@ import javax.servlet.ServletRegistration;
 /**
  * Created by svichkar on 6/8/2016.
  */
-public class SpringWebAppInitializer implements WebApplicationInitializer {
+public class SpringWebAppInitializer extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public UrlBasedViewResolver setupTilesViewResolver() {
+        UrlBasedViewResolver viewResolver = new UrlBasedViewResolver();
+        viewResolver.setViewClass(ApplicationContextConfig.class);
+        return viewResolver;
+    }
 
     @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
-
-        AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
-        appContext.register(ApplicationContextConfig.class);
-        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("SpringDispatcher", new DispatcherServlet(appContext));
-        dispatcher.setLoadOnStartup(1);
-        dispatcher.addMapping("/", "/static/*");
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/css/**").addResourceLocations("/static/css/");
+        registry.addResourceHandler("/static/html/**").addResourceLocations("/static/html/");
+        registry.addResourceHandler("/static/image/**").addResourceLocations("/image/css/");
+        registry.addResourceHandler("/static/js/**").addResourceLocations("/image/js/");
     }
 }
--- learn/src/main/webapp/index.jsp
+++ learn/src/main/webapp/index.jsp
@@ -1,6 +1,6 @@
 <html>
 <head>
-    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <link rel="stylesheet" type="text/css" href="static/css/style.css">
 </head>
 
 <body>

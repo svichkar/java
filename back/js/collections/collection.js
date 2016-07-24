0f0c68487dcf8e51cf6d6765b90341e2568ffc64
konstantin_svichkar <konstantin.svichkar@nixsolutions.com>
2016-07-24 23:52:45 +0300

    starting

---
+++ back/js/collections/collection.js
@@ -0,0 +1,10 @@
+define(['backbone', 'models/mainModel'], function (Backbone, MainModel) {
+    var Col = Backbone.Collection.extend({
+        model: MainModel,
+        url: 'json/response.json',
+        parse: function (data) {
+            return data.result;
+        }
+    });
+    return Col;
+});
